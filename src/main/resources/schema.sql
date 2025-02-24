8f69d8a2e4dff5841bd4d4a58a32634d1b526d5d
hyemee <svsvdvdv@naver.com>
2025-02-24 12:49:05 +0900

    fetch 를 이용한 회원가입 기능 구현 4 - 비밀번호에 대해 SHA-256 해시 생성

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 create table members (
     mno int auto_increment primary key,
     userid varchar(18) unique not null,
-    userpwd varchar(18) not null ,
+    userpwd varchar(64) not null ,
     name varchar(50) not null null,
     email varchar(100) not null,
     regdate datetime default  current_timestamp
