200cba7521b01175240e1426389f4d6d69318c6a
hyemee <svsvdvdv@naver.com>
2025-02-27 10:26:32 +0900

    게시판 list 구현 8 - 페이지네이션 구현 2 : cpg에 따라 페이지블럭 출력

--- src/main/java/com/example/svsvdvdv/semiprojectv1/controller/BoardController.java
+++ src/main/java/com/example/svsvdvdv/semiprojectv1/controller/BoardController.java
@@ -26,6 +26,8 @@ public class BoardController {
 
         m.addAttribute("bds", boardService.readBoard(cpg));
         m.addAttribute("cpg", cpg);
+        m.addAttribute("stblk", ((cpg - 1) / 10) * 10 + 1);
+
 
         return "views/board/list";
     }
