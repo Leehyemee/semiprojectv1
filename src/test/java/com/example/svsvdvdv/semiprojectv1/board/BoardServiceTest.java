55ba10b55b73f84c12e9f2a1d9fc25ecfd84f9d0
hyemee <svsvdvdv@naver.com>
2025-03-06 11:23:00 +0900

    게시판 본문보기 개선 - BoardReplyDTO를 이용해서 본문/댓글 출력을 서비스 단위로 통합 - 트랜젝션 애노테이션 추가

--- src/test/java/com/example/svsvdvdv/semiprojectv1/board/BoardServiceTest.java
+++ src/test/java/com/example/svsvdvdv/semiprojectv1/board/BoardServiceTest.java
@@ -3,6 +3,7 @@ package com.example.svsvdvdv.semiprojectv1.board;
 import com.example.svsvdvdv.semiprojectv1.domain.Board;
 import com.example.svsvdvdv.semiprojectv1.domain.BoardDTO;
 import com.example.svsvdvdv.semiprojectv1.domain.BoardListDTO;
+import com.example.svsvdvdv.semiprojectv1.domain.BoardReplyDTO;
 import com.example.svsvdvdv.semiprojectv1.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.assertj.core.api.AssertionsForClassTypes;
@@ -83,11 +84,12 @@ public class BoardServiceTest {
         int bno = 3006;
 
         // when
-        Board result = boardService.readOneBoard(bno);
+        //Board result = boardService.readOneBoard(bno);
+        BoardReplyDTO result = boardService.readOneBoardReply(bno);
 
         // Then
         assertThat(result).isNotNull();
-        assertThat(result.getUserid()).isNotNull();
+        assertThat(result.getBd().getUserid()).isNotNull();
 
     }
 }
