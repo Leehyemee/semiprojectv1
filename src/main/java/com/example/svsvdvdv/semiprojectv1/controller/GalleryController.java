154a2b486516fa9d0034d2a470eb5c604b8e1271
hyemee <svsvdvdv@naver.com>
2025-03-06 12:46:58 +0900

    빠트린 내용 추가함 갤러리 구현 2 - Gallery 클래스 정의 GalleryService, GalleryRepository 구현

--- src/main/java/com/example/svsvdvdv/semiprojectv1/controller/GalleryController.java
+++ src/main/java/com/example/svsvdvdv/semiprojectv1/controller/GalleryController.java
@@ -1,5 +1,6 @@
 package com.example.svsvdvdv.semiprojectv1.controller;
 
+import com.example.svsvdvdv.semiprojectv1.service.GalleryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/gallery")
 public class GalleryController {
 
+    private final GalleryService galleryService;
+
+    public GalleryController(GalleryService galleryService) {
+        this.galleryService = galleryService;
+    }
+
     @GetMapping("/list")
-    public String list() {
+    public String list(Model m) {
+
         return "views/gallery/list";
     }
 }
