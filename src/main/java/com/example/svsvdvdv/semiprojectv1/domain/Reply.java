f8660c723414b7160d3ecfb1fd8a2033636ee23f
hyemee <svsvdvdv@naver.com>
2025-04-01 09:19:04 +0900

    기타 수정

--- src/main/java/com/example/svsvdvdv/semiprojectv1/domain/Reply.java
+++ src/main/java/com/example/svsvdvdv/semiprojectv1/domain/Reply.java
@@ -3,13 +3,15 @@ package com.example.svsvdvdv.semiprojectv1.domain;
 import lombok.Builder;
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 @Builder
 public class Reply {
     private String rno;
     private String comments;
     private String userid;
-    private String regdate;
+    private LocalDateTime regdate;
     private String ref;
     private String pno;
 }
